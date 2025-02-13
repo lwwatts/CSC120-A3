49e93c49b514584005bb43ca13197197dac537d2
lwwatts <lwwatts@smith.edu>
2025-02-13 11:02:19 -0500

    Filled out method chat(), but not working  right

--- Conversation.java
+++ Conversation.java
@@ -1,27 +1,41 @@
 
+import java.util.Scanner;
+
 class Conversation implements Chatbot {
 
   // Attributes 
+  String transcript;
 
   /**
    * Constructor 
    */
   Conversation() {
-    
+    transcript = "\nHi there! What's on your mind?";
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
-
+    Scanner in = new Scanner(System.in);
+    System.out.println("How many rounds? ");
+    int rounds = in.nextInt();
+    System.out.println(transcript);
+    while(rounds > 0){
+      String statement = in.nextLine();
+      transcript += "\n" + statement;
+      String response = respond(statement);
+      System.out.println(response);
+      transcript += "\n" + response;
+      rounds -= 1;
+    }
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+    System.out.println(transcript);
   }
 
   /**
@@ -30,7 +44,7 @@ class Conversation implements Chatbot {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
+    String returnString = "Mhm."; 
     return returnString; 
   }
 
